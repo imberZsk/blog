86945b4db3f97077fa47e48e637f2075f742d15c
imber <a1157911285@163.com>
2024-02-25 21:30:34 +0800

    feat: 🚀国际化

--- src/app/[lang]/(foreground)/layout.tsx
+++ src/app/[lang]/(foreground)/layout.tsx
@@ -1,15 +1,16 @@
 import type { Metadata } from 'next'
 import '@/app/globals.css'
 import Nav from '@/ui/foreground/nav'
+import { Locale } from '../../../../i18n-config'
 
 export const metadata: Metadata = {
   title: 'foreground ｜ imber docs'
 }
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default function Layout({ children, params }: { children: React.ReactNode; params: { lang: Locale } }) {
   return (
     <main>
-      <Nav></Nav>
+      <Nav params={params}></Nav>
       {children}
     </main>
   )
--- src/ui/foreground/nav.tsx
+++ src/ui/foreground/nav.tsx
@@ -1,22 +1,27 @@
+import { getDictionary } from '../../../get-dictionary'
+import { Locale } from '../../../i18n-config'
+import LocaleSwitcher from './locale-switcher'
 import ThemeButton from '../common/theme-button'
 import Link from 'next/link'
 
-export default function Nav() {
+export default async function Nav({ params: { lang } }: { params: { lang: Locale } }) {
+  const dictionary = await getDictionary(lang)
+
   return (
     <nav className="flex items-center justify-between px-[48px] py-[32px]">
       <Link href={'/'}>imber</Link>
       <div className="flex items-center gap-4">
-        <Link href={'/virtual-list'}>虚拟列表</Link>
-        <Link href={'/waterfall'}>瀑布流</Link>
-        <a href="https://imber-docs.netlify.app" target="_blank">
-          文档
-        </a>
-        <Link href={'/blog'}>博客</Link>
-        <Link href={'/editor'} target="_blank">
-          写文章
-        </Link>
+        {dictionary.tabs.map((item, index) => {
+          return (
+            <Link href={item.href} key={index} target={item.target}>
+              {item.name}
+            </Link>
+          )
+        })}
         <ThemeButton></ThemeButton>
-        <div className="cursor-pointer">切换语言</div>
+        <div className="cursor-pointer">
+          <LocaleSwitcher></LocaleSwitcher>
+        </div>
         <Link className="h-[30px] w-[30px] rounded-full bg-[#000000] dark:bg-[#ffffff]" href={'login'}></Link>
       </div>
     </nav>
