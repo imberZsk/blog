8ac5063dba1398f54f32ce26c7d418a3a9afc83d
imber <a1157911285@163.com>
2024-03-05 18:25:16 +0800

    feat: 🚀虚拟列表

--- src/app/[lang]/(foreground)/virtual-list/page.tsx
+++ src/app/[lang]/(foreground)/virtual-list/page.tsx
@@ -4,7 +4,7 @@ import React, { useState, useEffect, useRef } from 'react'
 
 const VirtualList = () => {
   const [startIndex, setStartIndex] = useState(0)
-  const [endIndex, setEndIndex] = useState(9)
+  const [endIndex, setEndIndex] = useState(7)
   const containerRef = useRef<HTMLDivElement | null>(null)
 
   useEffect(() => {
@@ -15,11 +15,12 @@ const VirtualList = () => {
     const handleScroll = () => {
       const scrollTop = container.scrollTop
       const clientHeight = container.clientHeight
-      const visibleItems = Math.ceil(clientHeight / 60) // 每个列表项高度为 60px
+      const visibleItems = Math.ceil(clientHeight / (60 + 20)) // 每个列表项高度为 60px
+      console.log(visibleItems)
       const totalItems = 100 // 列表项总数
 
-      const newStartIndex = Math.floor(scrollTop / 60)
-      const newEndIndex = Math.min(newStartIndex + visibleItems - 1, totalItems - 1)
+      const newStartIndex = Math.max(Math.floor(scrollTop / (60 + 20)) - 2, 0)
+      const newEndIndex = Math.min(newStartIndex + visibleItems + 4, totalItems - 1)
 
       setStartIndex(newStartIndex)
       setEndIndex(newEndIndex)
@@ -32,8 +33,8 @@ const VirtualList = () => {
   }, [])
 
   return (
-    <div ref={containerRef} className="mx-auto mt-[200px] h-[600px] w-[800px] overflow-hidden overflow-y-auto">
-      <div style={{ height: `${startIndex * 60}px` }}></div>
+    <div ref={containerRef} className="mx-auto mt-[200px] h-[640px] w-[800px] overflow-hidden overflow-y-auto">
+      <div style={{ height: `${startIndex * (60 + 20)}px` }}></div>
       {Array(endIndex - startIndex + 1)
         .fill(0)
         .map((_, index) => {
@@ -44,7 +45,7 @@ const VirtualList = () => {
             </div>
           )
         })}
-      <div style={{ height: `${(100 - endIndex - 1) * 60}px` }}></div>
+      <div style={{ height: `${(100 - endIndex - 1) * (60 + 20)}px` }}></div>
     </div>
   )
 }
